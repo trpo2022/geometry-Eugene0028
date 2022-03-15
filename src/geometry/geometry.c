64b921974bd1ea9272a6211f1caf59e4fe82f966
Eugene0028 <Eugene0028@users.noreply.github.com>
2022-03-16 03:50:30 +0700

    The new era

--- src/geometry/geometry.c
+++ src/geometry/geometry.c
@@ -1,36 +1,30 @@
-#include <stdio.h>
-#include <math.h>
 #include "libgeo/circle.h"
-#include "libgeo/triangle.h"
 #include "libgeo/poligon.h"
+#include "libgeo/triangle.h"
+#include <math.h>
+#include <stdio.h>
 
-int main() 
+int main()
 {
-	int a;
-	while(1)
-	{
-		printf("1. Cirle\n2. Triangle\n3. Poligon\nChoose your figure: ");
-		scanf(" %d", &a);
-		if (a == 1)
-		{
-			circle();
-			break;
-		}
-		if (a == 2)
-		{
-			triangle();
-			break;
-		}
-		if (a == 3)
-		{
-			poligon();
-			break;
-		}
-		else
-		{
-			printf("Error\n");
-			return 0;
-		}
-	}
-	return 0;
+    int a;
+    while (1) {
+        printf("1. Cirle\n2. Triangle\n3. Poligon\nChoose your figure: ");
+        scanf(" %d", &a);
+        if (a == 1) {
+            circle();
+            break;
+        }
+        if (a == 2) {
+            triangle();
+            break;
+        }
+        if (a == 3) {
+            poligon();
+            break;
+        } else {
+            printf("Error\n");
+            return 0;
+        }
+    }
+    return 0;
 }
--- src/libgeo/poligon.c
+++ src/libgeo/poligon.c
@@ -1,27 +1,21 @@
-#include <stdio.h>
-#include <math.h>
 #include "poligon.h"
+#include <math.h>
+#include <stdio.h>
 
 void poligon()
 {
     float a, b, c, d, p, s;
     printf("Enter (a, b, c, d) for poligon: ");
     scanf(" poligon(%f, %f, %f, %f)", &a, &b, &c, &d);
-    if (a > 0 &&  b > 0 && c > 0 && d > 0)
-    {
-        if (a == c && b == d )
-        {
+    if (a > 0 && b > 0 && c > 0 && d > 0) {
+        if (a == c && b == d) {
             p = (a + b) * 2;
             s = a * b;
             printf("Perimetr: %.2f\nS: %.2f\n", p, s);
-        }
-        else
-        {
+        } else {
             printf("Error\n");
         }
-    }
-    else
-    {
+    } else {
         printf("Error\n");
     }
 }
