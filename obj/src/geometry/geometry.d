15f1573b29e905a7c445b0b2df67ee6a10d58dd6
Eugene0028 <Eugene0028@users.noreply.github.com>
2022-03-16 22:49:18 +0700

    Fix all

--- obj/src/geometry/geometry.d
+++
@@ -1,8 +0,0 @@
-obj/src/geometry/geometry.o: src/geometry/geometry.c src/libgeo/circle.h \
- src/libgeo/triangle.h src/libgeo/poligon.h
-
-src/libgeo/circle.h:
-
-src/libgeo/triangle.h:
-
-src/libgeo/poligon.h:
