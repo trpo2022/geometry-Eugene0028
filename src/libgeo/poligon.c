7c895c913322a3f812c8a24e4605efdb8e22c32d
Eugene0028 <Eugene0028@users.noreply.github.com>
2022-03-17 22:43:45 +0700

    Fix

--- src/libgeo/poligon.c
+++ src/libgeo/poligon.c
@@ -4,24 +4,18 @@
 
 void poligon()
 {
-	float a, b, c, d, p, s;
-	printf("Enter poligon(a, b, c, d)\n");
-	scanf(" poligon(%f, %f, %f, %f)", &a, &b, &c, &d);
-	if (a > 0 && b > 0 && c > 0 && d > 0) 
-	{
-		if (a == c && b == d) 
-		{
-            		p = (a + b) * 2;
-            		s = a * b;
-            		printf("Perimetr: %.2f\nS: %.2f\n", p, s);
-		} 
-		else 
-		{
-			printf("Error\n");
-		}
-	} 
-	else 
-	{
-        	printf("Error\n");
-	}
+    float a, b, c, d, p, s;
+    printf("Enter poligon(a, b, c, d)\n");
+    scanf(" poligon(%f, %f, %f, %f)", &a, &b, &c, &d);
+    if (a > 0 && b > 0 && c > 0 && d > 0) {
+        if (a == c && b == d) {
+            p = (a + b) * 2;
+            s = a * b;
+            printf("Perimetr: %.2f\nS: %.2f\n", p, s);
+        } else {
+            printf("Error\n");
+        }
+    } else {
+        printf("Error\n");
+    }
 }
